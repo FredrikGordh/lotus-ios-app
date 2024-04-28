73dac0b6b5bdf16e6203a1ec4062991cf0bb23f0
Fredrik Riseby <fredrik.gordh@weknowit.se>
2024-04-28 20:28:05 +0200

    Initial Commit

---
+++ Lotus/LotusApp.swift
@@ -0,0 +1,17 @@
+//
+//  LotusApp.swift
+//  Lotus
+//
+//  Created by Fredrik Gordh Riseby on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct LotusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
